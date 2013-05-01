ff0e672f4efeeb65308d7394f5e2711b8dbcac44
trance-with-me <thequickandthedead@gmail.com>
2013-04-30 17:45:09 -0700

    Create program to generate complex makefile
    Create test2.c which reads a listing of file names from a text file and
    generates a higher-level makefile. Compile test2.c, run a.out, and make.

---
+++ hello/test2.c
@@ -0,0 +1,100 @@
+#include <string.h>
+#include <stdio.h>
+
+int main() 
+{
+    /* 20 consecutive arrays of 15 characters each */    
+    char file_array[20][15] = {'\0'};
+    char file_name[15]      = {'\0'};
+    int i                   = 0;
+    int num_files           = 0;
+
+    /* open file for reading */
+    FILE* input_text = fopen ("files.txt", "r");
+    /* open file for writing */
+    FILE* makefile = fopen("Makefile", "wb");
+
+    if (input_text == NULL)
+    {
+        perror("Error opening file");
+    }
+    else 
+    {
+        while (fgets(file_name, 15, input_text) != NULL)
+        {
+            printf("[%d] read a file: %s", i, file_name);
+
+            /* copy file name (static string) into array */
+            strcpy(file_array[i], file_name);
+            i++;
+        }
+
+        /* save total number of files read */
+        num_files = i;
+
+        /* close the file */
+        fclose(input_text);
+
+        /* print out array */
+        for (i = 0; i < num_files; ++i)
+        {
+            printf("Array[%d]: %s", i, file_array[i]);
+        }
+
+        /* flags and options can be modifiable later */
+        fprintf(makefile, "CC = gcc\n");
+        fprintf(makefile, "CFLAGS = -I.\n");
+        fprintf(makefile, "DEPS =");
+
+        /* print out all .h files */
+        int j = 0;
+        char h[] = "h";
+
+        /* print out the .h files */
+        for (i = 0; i < num_files; ++i)
+        {
+            /* remove '\n' at the end of each string */
+            file_array[i][strlen(file_array[i])-1] = '\0';
+            if (file_array[i][strlen(file_array[i])-1] == 'h')
+            {
+                fprintf(makefile, " %s", file_array[i]);
+            }
+        }
+        fprintf(makefile, "\nOBJ =");
+
+        /* print out .c files (replace .c with .o) */
+        for (i = 0; i < num_files; ++i)
+        {
+            if (file_array[i][strlen(file_array[i])-1] == 'c')
+            {
+                file_array[i][strlen(file_array[i])-1] = 'o';
+                file_array[i][strlen(file_array[i])-2] = '.';
+                fprintf(makefile, " %s", file_array[i]);
+            }
+        }
+        /* to print % add a % before it to escape */
+        fprintf(makefile, "\n\n%%.o: %%.c $(DEPS)");
+        fprintf(makefile, "\n\t$(CC) -c -o $@ $< $(CFLAGS)");
+        fprintf(makefile, "\n\nrunnable: $(OBJ)");
+        fprintf(makefile, "\n\t$(CC) -o $@ $^ $(CFLAGS)");
+        fprintf(makefile, "\n\nclean:");
+        fprintf(makefile, "\n\trm -f runnable $(OBJ)\n");
+    }
+    return 0;
+}
+
+/* 
+CC = gcc
+CFLAGS = -I.
+DEPS = hello.h hello1.h hello2.h hello3.h
+OBJ = hello.o hello1.o hello2.o hello3.o main.o
+
+%.o: %.c $(DEPS)
+    $(CC) -c -o $@ $< $(CFLAGS)
+
+runnable: $(OBJ)
+    $(CC) -o $@ $^ $(CFLAGS)
+
+clean:
+    rm -f runnable $(OBJ)
+*/
