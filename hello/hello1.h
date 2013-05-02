7561796678af8d677b95f083df3387fdfe684ae0
trance-with-me <thequickandthedead@gmail.com>
2013-05-01 20:17:36 -0700

    Remove hello folder

--- hello/hello1.h
+++
@@ -1,8 +0,0 @@
-#ifndef HELLO1_H
-#define HELLO1_H
-
-#include <stdio.h>
- 
-void my_print_func1(); 
- 
-#endif
--- hello/hello2.c
+++
@@ -1,6 +0,0 @@
-#include "hello2.h"
-
-void my_print_func2() 
-{
-    printf("Hello, world2!\n");
-}
