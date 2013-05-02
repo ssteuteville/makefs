1b9821573341429bc34d69b99b18e7a757157593
trance-with-me <thequickandthedead@gmail.com>
2013-05-01 20:13:49 -0700

    Overhaul
    Compile driver_cmd_args.c and run as "./a.out <CFLAGS>"; specifying any
    flags as arguments. The driver now determines if the files in the
    directory are .c or .cpp. Need to add functionality to actually compile
    with g++.

---
+++ hello_files/hello2.h
@@ -0,0 +1,8 @@
+#ifndef HELLO2_H
+#define HELLO2_H
+
+#include <stdio.h>
+ 
+void my_print_func2();
+ 
+#endif
---
+++ hello_files/hello3.c
@@ -0,0 +1,6 @@
+#include "hello3.h"
+
+void my_print_func3() 
+{
+    printf("Hello, world3!\n");
+}
